3f25e73851aa1929d57c433ea10f3ca2fba7558e
glherme <guilhermefrancisco9@hotmail.com>
2026-08-23 14:35:52 -0300

    corrigindo erros de sintaxe e de execução

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,10 +1,11 @@
 --- ENUMS ---
 CREATE TYPE status_leito AS ENUM ('OCUPADO', 'LIVRE', 'MANUTENCAO');
 CREATE TYPE cargo_usuario AS ENUM ('MEDICO', 'ENFERMAGEM', 'MANUTENCAO');
-CREATE TYPE tipo_sensor AS ENUM ('PACIENTE', 'LEITO')
+CREATE TYPE tipo_sensor AS ENUM ('PACIENTE', 'LEITO');
 -------------------------------------------------------------------------
+
 --- GERENTE ---
-CREATE TABLE IF NO EXISTS gerente(
+CREATE TABLE IF NOT EXISTS gerente(
     id_gerente SERIAL PRIMARY KEY,
     nome varchar(255) NOT NULL,
     cpf varchar(11) NOT NULL UNIQUE,
@@ -13,7 +14,7 @@ CREATE TABLE IF NO EXISTS gerente(
 );
 
 --- CPF CADASTRADO ---
-CREATE TABLE IF NO EXISTS cpf_cadastrado(
+CREATE TABLE IF NOT EXISTS cpf_cadastrado(
     cpf VARCHAR(11) PRIMARY KEY,
     cargo cargo_usuario NOT NULL,
     data_habilitado TIMESTAMP NOT NULL,
@@ -21,72 +22,74 @@ CREATE TABLE IF NO EXISTS cpf_cadastrado(
 );
 
 --- USUARIO ---
-CREATE TABLE IF NO EXISTS usuario(
-    id_usuario INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS usuario(
+    id_usuario SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE REFERENCES cpf_cadastrado(cpf),
     login VARCHAR(255) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    cargo cargo_usuario NOT NULL,
+    cargo cargo_usuario NOT NULL
 );
 
 --- MEDICO ---
-CREATE TABLE IF NO EXISTS medico(
+CREATE TABLE IF NOT EXISTS medico(
     id_medico INT PRIMARY KEY REFERENCES usuario(id_usuario)
 );
 
 --- MANUTENCAO ---
-CREATE TABLE IF NO EXISTS manutencao(
+CREATE TABLE IF NOT EXISTS manutencao(
     id_manutencao INT PRIMARY KEY REFERENCES usuario(id_usuario)
-)
+);
+
 --- ENFERMAGEM ---
-CREATE TABLE IF NO EXISTS enfermagem(
+CREATE TABLE IF NOT EXISTS enfermagem(
     id_enfermagem INT PRIMARY KEY REFERENCES usuario(id_usuario)
 );
 
 ------ PACIENTE -----
-CREATE TABLE IF NO EXISTS paciente(
-    id_paciente INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS paciente(
+    id_paciente SERIAL PRIMARY KEY,
     nome_completo VARCHAR(255) NOT NULL,
     cpf VARCHAR(11) NOT NULL UNIQUE,
-    data_nascimento DATE NOT NULL,
+    data_nascimento DATE NOT NULL
 );
 
 ----- LEITO -----
-CREATE TABLE IF NO EXISTS leito(
-    id_leito INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS leito(
+    id_leito SERIAL PRIMARY KEY,
     numero_leito INT NOT NULL UNIQUE,
     localizacao VARCHAR(100) NOT NULL,
     status_leito status_leito NOT NULL DEFAULT 'LIVRE',
-    id_paciente INT UNIQUE NOT NULL REFERENCES paciente(id_paciente)
-)
+    id_paciente INT UNIQUE REFERENCES paciente(id_paciente)
+);
+
 ----- SENSOR -----
-CREATE TABLE IF NO EXISTS sensor(
+CREATE TABLE IF NOT EXISTS sensor(
     id_sensor VARCHAR(30) PRIMARY KEY,
     intervalo_leitura INT NOT NULL,
     tipo tipo_sensor NOT NULL
 );
 
 ----- SENSOR PACIENTE -----
-CREATE TABLE IF NO EXISTS sensor_paciente(
+CREATE TABLE IF NOT EXISTS sensor_paciente(
     id_sensor VARCHAR(30) PRIMARY KEY REFERENCES sensor(id_sensor),
     id_paciente INT NOT NULL REFERENCES paciente(id_paciente),
-    pinos_i2c INT NOT NULL,
-    pinos_one_wire INT NOT NULL
+    pinos_i2c INT,
+    pinos_one_wire INT
 );
 
 ----- SENSOR AMBIENTE ------
-CREATE TABLE IF NO EXISTS sensor_ambiente(
+CREATE TABLE IF NOT EXISTS sensor_ambiente(
     id_sensor VARCHAR(30) PRIMARY KEY REFERENCES sensor(id_sensor),
     id_leito INT NOT NULL REFERENCES leito(id_leito),
-    pinos_sda_scl_ INT NOT NULL,
-    pino_analogico INT NOT NULL
+    pinos_sda_scl INT,
+    pino_analogico INT
 );
 
 ----- LEITURA PACIENTE -----
 -- Sensores: MAX30102 (spo2/bpm)
-CREATE TABLE IF NO EXISTS leitura_paciente(
-    id_leitura INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS leitura_paciente(
+    id_leitura SERIAL PRIMARY KEY,
     id_sensor VARCHAR(30) NOT NULL REFERENCES sensor_paciente(id_sensor),
     id_paciente INT NOT NULL REFERENCES paciente(id_paciente),
     data_leitura TIMESTAMP NOT NULL,
@@ -98,8 +101,8 @@ CREATE TABLE IF NO EXISTS leitura_paciente(
 ----- LEITURA AMBIENTE
 -- Sensores: BME280 (temperatura), ENS160 (particulas no ar), KY-037 (ruido)
 -- luminosidade
-CREATE TABLE IF NO EXISTS leitura_ambiente(
-    id_leitura INT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS leitura_ambiente(
+    id_leitura SERIAL PRIMARY KEY,
     id_sensor VARCHAR(30) NOT NULL REFERENCES sensor_ambiente(id_sensor),
     id_leito INT NOT NULL REFERENCES leito(id_leito),
     data_leitura TIMESTAMP NOT NULL,
@@ -107,7 +110,7 @@ CREATE TABLE IF NO EXISTS leitura_ambiente(
     umidade_ambiente FLOAT,
     pressao_ambiente FLOAT,
     indice_tvoc INT,
-    idice_eco2 INT,
+    indice_eco2 INT,
     nivel_ruido FLOAT,
     nivel_luminosidade FLOAT
 );
