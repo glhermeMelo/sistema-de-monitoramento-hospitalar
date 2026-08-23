fce9869f780db1cfa259218c872e34098e9e6e5e
glherme <guilhermefrancisco9@hotmail.com>
2026-08-22 21:54:42 -0300

    inicializando repositório

---
+++ src/main/java/br/edu/ufersa/smh/SmhApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.smh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/ufersa/smh/SmhApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufersa.smh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
