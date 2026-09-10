c3d493bf39ee8ab70c89d732e2b8bce198b30a1c
lavicrw <laviniatail@gmail.com>
2026-09-10 19:45:27 -0300

    Minha mensagem de alteracao

--- src/main/java/br/edu/ufersa/smh/leitura/controller/LeituraController.java
+++ src/main/java/br/edu/ufersa/smh/leitura/controller/LeituraController.java
@@ -1,9 +1,61 @@
 package br.edu.ufersa.smh.leitura.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ufersa.smh.leitura.dto.LeituraDTO;
+import br.edu.ufersa.smh.leitura.dto.LeituraResponse;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/leituras")
 public class LeituraController {
+
+    @GetMapping
+    public ResponseEntity<List<LeituraResponse>> listarLeituras() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<LeituraResponse> cadastrarLeitura(
+            @RequestBody LeituraDTO dto) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LeituraResponse> detalharLeitura(
+            @PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LeituraResponse> atualizarLeituraTotal(
+            @PathVariable Long id,
+            @RequestBody LeituraDTO dto) {
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<LeituraResponse> atualizarLeituraParcial(
+            @PathVariable Long id,
+            @RequestBody LeituraDTO dto) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarLeitura(
+            @PathVariable Long id) {
+        return null;
+    }
 }
--- src/main/java/br/edu/ufersa/smh/sensor/controller/SensorController.java
+++ src/main/java/br/edu/ufersa/smh/sensor/controller/SensorController.java
@@ -1,7 +1,61 @@
 package br.edu.ufersa.smh.sensor.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ufersa.smh.sensor.dto.SensorDTO;
+import br.edu.ufersa.smh.sensor.dto.SensorResponse;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/sensores")
 public class SensorController {
+
+    @GetMapping
+    public ResponseEntity<List<SensorResponse>> listarSensores() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<SensorResponse> cadastrarSensor(
+            @RequestBody SensorDTO dto) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SensorResponse> detalharSensor(
+            @PathVariable Long id) {
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<SensorResponse> atualizarSensorTotal(
+            @PathVariable Long id,
+            @RequestBody SensorDTO dto) {
+        return null;
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<SensorResponse> atualizarSensorParcial(
+            @PathVariable Long id,
+            @RequestBody SensorDTO dto) {
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarSensor(
+            @PathVariable Long id) {
+        return null;
+    }
 }
