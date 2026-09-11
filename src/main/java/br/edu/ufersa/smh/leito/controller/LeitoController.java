69ce189c5e54acbe7eb8274eac93edf159be2a66
KarenSGomes <karen.gomes@alunos.ufersa.edu.br>
2026-09-10 21:13:27 -0300

    feat: implementa controllers de leito e paciente

--- src/main/java/br/edu/ufersa/smh/leito/controller/LeitoController.java
+++ src/main/java/br/edu/ufersa/smh/leito/controller/LeitoController.java
@@ -1,26 +1,62 @@
 package br.edu.ufersa.smh.leito.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.edu.ufersa.smh.leito.dto.LeitoDTO;
+import br.edu.ufersa.smh.leito.dto.LeitoPatchDTO;
 import br.edu.ufersa.smh.leito.dto.LeitoResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/leitos")
 public class LeitoController {
+
     @GetMapping
     public ResponseEntity<List<LeitoResponse>> listarLeitos() {
         return null;
     }
 
+    @PostMapping
+    public ResponseEntity<LeitoResponse> cadastrarLeito(
+            @RequestBody LeitoDTO dto) {
+        return null;
+    }
+
     @GetMapping("/{idLeito}")
-    public ResponseEntity<LeitoResponse> buscarLeito(
+    public ResponseEntity<LeitoResponse> detalharLeito(
+            @PathVariable Integer idLeito) {
+        return null;
+    }
+
+    @PutMapping("/{idLeito}")
+    public ResponseEntity<LeitoResponse> atualizarLeitoTotal(
             @PathVariable Integer idLeito,
             @RequestBody LeitoDTO dto) {
+        return null;
+    }
+
+    @PatchMapping("/{idLeito}")
+    public ResponseEntity<LeitoResponse> atualizarLeitoParcial(
+            @PathVariable Integer idLeito,
+            @RequestBody LeitoPatchDTO dto) {
+        return null;
+    }
+
+    @DeleteMapping("/{idLeito}")
+    public ResponseEntity<Void> removerLeito(
+            @PathVariable Integer idLeito) {
         return null;
     }
 }
--- src/main/java/br/edu/ufersa/smh/paciente/controller/PacienteController.java
+++ src/main/java/br/edu/ufersa/smh/paciente/controller/PacienteController.java
@@ -1,10 +1,62 @@
 package br.edu.ufersa.smh.paciente.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ufersa.smh.paciente.dto.PacienteDTO;
+import br.edu.ufersa.smh.paciente.dto.PacientePatchDTO;
+import br.edu.ufersa.smh.paciente.dto.PacienteResponse;
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/pacientes")
 public class PacienteController {
 
+    @GetMapping
+    public ResponseEntity<List<PacienteResponse>> listarPacientes() {
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<PacienteResponse> cadastrarPaciente(
+            @RequestBody PacienteDTO dto) {
+        return null;
+    }
+
+    @GetMapping("/{idPaciente}")
+    public ResponseEntity<PacienteResponse> detalharPaciente(
+            @PathVariable Integer idPaciente) {
+        return null;
+    }
+
+    @PutMapping("/{idPaciente}")
+    public ResponseEntity<PacienteResponse> atualizarPacienteTotal(
+            @PathVariable Integer idPaciente,
+            @RequestBody PacienteDTO dto) {
+        return null;
+    }
+
+    @PatchMapping("/{idPaciente}")
+    public ResponseEntity<PacienteResponse> atualizarPacienteParcial(
+            @PathVariable Integer idPaciente,
+            @RequestBody PacientePatchDTO dto) {
+        return null;
+    }
+
+    @DeleteMapping("/{idPaciente}")
+    public ResponseEntity<Void> removerPaciente(
+            @PathVariable Integer idPaciente) {
+        return null;
+    }
 }
