2023dc29148a1d5acac9e0edd5839bcb74786926
lavicrw <laviniatail@gmail.com>
2026-09-17 17:15:17 -0300

    leitura ambiente e paciente

--- src/main/java/br/edu/ufersa/smh/leitura/model/LeituraAmbiente.java
+++ src/main/java/br/edu/ufersa/smh/leitura/model/LeituraAmbiente.java
@@ -1,15 +1,51 @@
 package br.edu.ufersa.smh.leitura.model;
 
+import java.time.LocalDateTime;
+
+import br.edu.ufersa.smh.leito.model.Leito;
+import br.edu.ufersa.smh.sensor.model.SensorAmbiente;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
+@Entity
+@Table(name = "leitura_ambiente")
 @Getter
-public class LeituraAmbiente extends Leitura {
-    private String idLeito;
+public class LeituraAmbiente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_leitura")
+    private Integer idLeitura;
+
+
+    @Column(name = "data_leitura", nullable = false)
+    private LocalDateTime dataLeitura;
+
+    @Column(name = "temperatura_ambiente")
     private Float temperaturaAmbiente;
+
+    @Column(name = "umidade_ambiente")
     private Float umidadeAmbiente;
+
+    @Column(name = "pressao_ambiente")
     private Float pressaoAmbiente;
+
+    @Column(name = "indice_tvoc")
     private Integer indiceTvoc;
+
+    @Column(name = "indice_eco2")
     private Integer indiceEco2;
+
+    @Column(name = "nivel_ruido")
     private Float nivelRuido;
+
+    @Column(name = "nivel_luminosidade")
     private Float nivelLuminosidade;
 }
--- src/main/java/br/edu/ufersa/smh/leitura/model/LeituraPaciente.java
+++ src/main/java/br/edu/ufersa/smh/leitura/model/LeituraPaciente.java
@@ -1,11 +1,39 @@
 package br.edu.ufersa.smh.leitura.model;
 
+import java.time.LocalDateTime;
+
+import br.edu.ufersa.smh.paciente.model.Paciente;
+import br.edu.ufersa.smh.sensor.model.SensorPaciente;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
+@Entity
+@Table(name = "leitura_paciente")
 @Getter
-public class LeituraPaciente extends Leitura {
-    private String idPaciente;
+public class LeituraPaciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_leitura")
+    private Integer idLeitura;
+
+
+    @Column(name = "dados_leitura", nullable = false)
+    private LocalDateTime dadosLeitura;
+
+    @Column(name = "temperatura_corporal")
     private Float temperaturaCorporal;
+
+    @Column(name = "spo2")
     private Float spo2;
+
+    @Column(name = "bpm")
     private Integer bpm;
 }
